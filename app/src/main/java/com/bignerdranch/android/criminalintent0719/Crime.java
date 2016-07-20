9f80a4ca4ada0d98e90519abc3e9d208c23b1d3f
flambo <ieem52057@gmail.com>
2016-07-20 15:29:23 +0900

    Ch9 207p까지 CriminalIntent의 모델계층 변경하기

--- app/src/main/java/com/bignerdranch/android/criminalintent0719/Crime.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent0719/Crime.java
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.criminalintent0719;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -9,10 +10,14 @@ public class Crime {
 
     private UUID mId;
     private  String mTitle;
+    private Date mDate;//범죄가 발생한 날짜
+    private  boolean mSolved;//범죄가 해결되었는지 여부
+
 
     public Crime(){
         //고유한 식별자를 생성한다.
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public UUID getId() {
@@ -26,5 +31,21 @@ public class Crime {
 
     public void setTitle(String title) {
         mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
     }
 }
--- app/src/main/java/com/bignerdranch/android/criminalintent0719/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent0719/CrimeFragment.java
@@ -8,7 +8,13 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by 이임경 on 2016-07-20.
@@ -20,6 +26,8 @@ public class CrimeFragment extends Fragment{
 
     private  Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -47,13 +55,42 @@ public class CrimeFragment extends Fragment{
             public void onTextChanged(CharSequence s, int i, int i1, int i2) {
 
                 //Crime의 제목을 설정하기 위해 사용됨.
-               mCrime.setTitle(s.toString());
+
+                //onTextChanged(...)에서는 사용자가 입력한 데이터 값을 갖고 있는
+                //CharSequence객체의 toString()메소드를 호출한다. 그리고 이 메소드에서 반환하는
+                //문자열은 Crime의 제목을 설정하기 위해 사용된다.
+
+                mCrime.setTitle(s.toString());
+
+
+
 
 
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        mDateButton = (Button)v.findViewById(R.id.crime_date);//버튼의 객체 참조를 얻고
+     //   mDateButton.setText(mCrime.getDate().toString());//그것의 텍스트를 범죄 발생일자로 지정한 후
+        mDateButton.setEnabled(false);//비활성화 한다.
+
+
+        Date now = new Date();
+        mCrime.setDate(now);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("E요일,  MM dd,yyyy");
+        mDateButton.setText(sdf.format(now));
+
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //범죄 해결 여부 속성 값을 설정한다.
+                mCrime.setSolved(isChecked);
 
             }
         });
